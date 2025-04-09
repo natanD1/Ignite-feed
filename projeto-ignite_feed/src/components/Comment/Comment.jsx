95704145175d3b49ba1f5a3e7709ebb3905dce81
Natan Dourado <nsdourado12@gmail.com>
2025-04-09 16:52:27 -0300

    adição da funcionalidade de likes e bloqueio de criação de comentários vázios

--- projeto-ignite_feed/src/components/Comment/Comment.jsx
+++ projeto-ignite_feed/src/components/Comment/Comment.jsx
@@ -1,8 +1,20 @@
 import styles from './Comment.module.css'
 import {ThumbsUp, Trash} from "phosphor-react";
 import {Avatar} from "../Avatar/Avatar.jsx";
+import {useState} from "react";
 
-export function Comment({ comment }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/natanD1.png" />
@@ -13,18 +25,19 @@ export function Comment({ comment }) {
                             <strong>Natan Dourado</strong>
                             <time title="11 de Maio às 08:13" dateTime="2023-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
-                        <button>
-                            <Trash size={24} title="Deletar comentário"/>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24}/>
                         </button>
                     </header>
 
-                    <p>{comment}</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
 
